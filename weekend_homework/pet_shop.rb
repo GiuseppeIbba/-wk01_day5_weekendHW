4964793c3ffe19ad608f60cf49c1aecf7b1e5300
Giuseppe <giuseppe.ibba@hotmail.com>
2017-11-26 18:02:16 +0000

    3. Add_or_remove_cash__add tested

--- weekend_homework/pet_shop.rb
+++ weekend_homework/pet_shop.rb
@@ -6,6 +6,6 @@ def total_cash(total)
    return @pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash__add(total_cash, money_earned)
+def add_or_remove_cash(total_cash, money_earned)
   p @pet_shop[:admin][:total_cash] += money_earned
 end
